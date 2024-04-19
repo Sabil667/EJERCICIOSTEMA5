70d9332df7734e9ef91c633d823f54ba03eb84c2
Sabil667 <msabi@myuax.com>
2024-04-19 16:57:23 +0200

    .

---
+++ src/main/java/POTENCIA/Potencia.java
@@ -0,0 +1,11 @@
+package POTENCIA;
+
+public class Potencia {
+    public static int calcularPotencia(int x, int y) {
+        if (y == 0) {
+            return 1;
+        } else {
+            return x * calcularPotencia(x, y - 1);
+        }
+    }
+}
