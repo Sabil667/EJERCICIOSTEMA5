c894c24f597b8389384be8705057a7bc760fbc81
Sabil667 <msabi@myuax.com>
2024-04-20 16:01:45 +0200

    .

---
+++ src/main/java/Ejercicios/Combinatoria.java
@@ -0,0 +1,11 @@
+package Ejercicios;
+
+public class Combinatoria {
+    public static int calcularCombinatoria(int n, int k) {
+        if (k == 0 || k == n) {
+            return 1;
+        } else {
+            return calcularCombinatoria(n - 1, k - 1) + calcularCombinatoria(n - 1, k);
+        }
+    }
+}
