e751e13e5440173323d1a1f656d2cb19aa69531f
Sabil667 <msabi@myuax.com>
2024-04-20 02:19:43 +0200

    .

--- src/main/java/SumaNaturales.java
+++ src/main/java/SumaNaturales.java
@@ -1,2 +1,9 @@
 public class SumaNaturales {
+    public static int sumaRecursiva(int n) {
+        if (n <= 1) {
+            return n;
+        } else {
+            return n + sumaRecursiva(n - 1);
+        }
+    }
 }
